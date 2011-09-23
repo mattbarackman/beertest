ef3b86ae76c0115868dbb58c2adf6b9bf465ed91
Matt Barackman <matthew.barackman@gmail.com>
2011-09-23 18:53:13 -0500

    added some stuff again

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/mbarackman/Documents/django/firstapp/firstapp.db',                      # Or path to database file if using sqlite3.
-		'USER': '',                      # Not used with sqlite3.
+        'NAME': 'mbarackman',                      # Or path to database file if using sqlite3.
+	'USER': 'mbarackman',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -45,12 +45,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/mbarackman/webapps/staticmediapp/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://mbarackman.webfactional.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -140,5 +140,12 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache'
+        'LOCATION': 'unix://unix:<~/memcached.sock>',
     }
 }
--- urls.py
+++ urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^polls/', include('polls.urls')),
-    (r'^admin/', include(admin.site.urls)),
+   # (r'^polls/', include('polls.urls')),
+   # (r'^admin/', include(admin.site.urls)),
 )
